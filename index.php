3389bee472c0caa4ac288aab128f9268a1476345
AndreaCoggio94 <andreacoggio94@gmail.com>
2023-09-29 16:12:47 +0200

    added method

--- index.php
+++ index.php
@@ -5,22 +5,33 @@
         public $genre;
         public $original_language;
         public $year;
+        public $vote;
 
-        function __construct($_title,
-        $_genre,
-        $_original_language,
-        $_year
+        function __construct(string $_title,
+        string $_genre,
+        string $_original_language,
+        float $_year,
+        float $_vote
         ){
             $this->title = $_title;
             $this->genre = $_genre;
             $this->original_language = $_original_language;
             $this->year = $_year;
+            $this->setVote($_vote);
         }
-
+        public function setVote($vote){
+            if ($vote <= 2) {
+                $this->vote = "low";
+            } elseif ($vote <= 4 ) {
+                $this->vote = "average";
+            } else {
+                $this->vote = "masterpiece";
+            }
+        } 
 
     }
 
 
-
+    
     
 ?>
