dac1e95ed0f00842d009f48a89213bad242bdf8d
AndreaCoggio94 <andreacoggio94@gmail.com>
2023-09-29 16:23:26 +0200

    2. added two objects Movie and printed on screen

--- index.php
+++ index.php
@@ -2,8 +2,23 @@
     include_once __DIR__ . "/Models/Movie.php" ;
 
     
-    $movie1 = new Movie( "LOTR", "Fantasy", "English", "2001", "5") ;
-    var_dump($movie1);
+    $lotr = new Movie( "LOTR", "Fantasy", "English", "2001", "5") ;
     
+    $harry_potter = new Movie("Harry Potter", "Fantasy", "English", "2001", "4");
     
+
+    $film_list = [
+        $lotr,
+        $harry_potter,
+    ];
+
+
+    foreach ($film_list as $film) {
+        echo "<br>";
+        foreach ($film as $value){
+            echo "$value <br>";
+        };
+    };
+
+   
 ?>
