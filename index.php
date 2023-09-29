c513b9deda1343d8e3098db590a44fc7f0f2d380
AndreaCoggio94 <andreacoggio94@gmail.com>
2023-09-29 15:52:08 +0200

    added __construct

--- index.php
+++ index.php
@@ -5,6 +5,19 @@
         public $genre;
         public $original_language;
         public $year;
+
+        function __construct($_title,
+        $_genre,
+        $_original_language,
+        $_year
+        ){
+            $this->title = $_title;
+            $this->genre = $_genre;
+            $this->original_language = $_original_language;
+            $this->year = $_year;
+        }
+
+
     }
 
 
